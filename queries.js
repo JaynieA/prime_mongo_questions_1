8ef87049ca8b38aa108bbeab4c1c5cd4e2cebc76
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-04 16:27:55 -0600

    add questions

---
+++ queries.js
@@ -0,0 +1,13 @@
+// 1. Find documents that have awards.
+
+// 2. Find documents that don't have awards.
+
+// 3. Find documents that have contribs for OOP or UNIX.
+
+// 4. Find documents with "Turing Award" awards.
+
+// 5. Find documents with IDs between 3 and 7.
+
+// 6. Find documents with awards that were awarded before the year 2000.
+
+// 7. Find documents with birth dates, but no death dates.
